067845d781c0279ca41de079e68ce3757e75d873
Nathan Wold <natew79@gmail.com>
2019-09-13 17:56:15 -0500

    added basic styling and complete initial server setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const todoRouter = require('./routes/todo.router.js');
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/list', todoRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log( 'server up on: ', PORT);
+})
