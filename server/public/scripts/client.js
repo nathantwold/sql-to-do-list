8547a85ac2f59a82cf1c59773d8c4a42a444c6fd
Nathan Wold <natew79@gmail.com>
2019-09-14 09:54:23 -0500

    GET working, need to append to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,35 @@
 $(document).ready(onReady);
 
-function onReady(){
+function onReady() {
   $('#addTask').on('click', submitTask);
+  showTasks();
+}
+
+// PUT tasks
+function completeTask() {
+
+}
+
+// DELETE tasks
+function deleteTask() {
+
+}
+
+// GET tasks
+function showTasks() {
+  $.ajax({
+    type: 'GET',
+    url: '/list'
+  }).then(function (response) {
+    console.log(response);
+    // append to DOM
+  }).catch(function (error) {
+    console.log('error in GET', error);
+  });
 }
 
 // POST tasks
-function submitTask(){
+function submitTask() {
   let thisTask = {
     task: $('#taskIn').val()
   }
@@ -13,26 +37,11 @@ function submitTask(){
     type: 'POST',
     url: '/list',
     data: thisTask
-    }).then(function(response) {
-      console.log('Response from server:', response);
-      // renderTasks();
-    }).catch(function(error) {
-      console.log('Error in POST', error)
-      alert('unable to add to list at this time');
-    });
-}
-
-// GET tasks
-function renderTasks(){
-
-}
-
-// PUT tasks
-function completeTask(){
-
-}
-
-// DELETE tasks
-function deleteTask(){
-
+  }).then(function (response) {
+    console.log('Response from server:', response);
+    showTasks();
+  }).catch(function (error) {
+    console.log('Error in POST', error)
+    alert('unable to add to list at this time');
+  });
 }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,18 +2,28 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-router.post('/',  (req, res) => {
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "list";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('error getting list', error);
+        res.sendStatus(500);
+    });
+});
+
+router.post('/', (req, res) => {
     let newTask = req.body;
     console.log(`Adding task`, newTask);
     let queryText = `INSERT INTO "list" ("task")
                      VALUES ($1);`;
-    pool.query(queryText, [newTask.task])
-      .then((result) => {
-        res.sendStatus(201);
-      }).catch((error) => {
+    pool.query(queryText, [newTask.task]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
         console.log(`Error adding new task`, error);
         res.sendStatus(500);
-      })
-  });
+    })
+});
 
 module.exports = router;
